370046e316a2de50b0b7e448771ce5e8dff55743
phsbido <pedrobido.ph@gmail.com>
2026-02-18 23:40:35 -0300

    initial commit

---
+++ src/test/java/com/pbido/booktrack/BooktrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbido.booktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
